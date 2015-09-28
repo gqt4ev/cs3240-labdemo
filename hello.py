89ce3ed31fe9002ce7de3b0da334b3c7b4e8cb7e
Garnall Travis <gqt4ev@virginia.edu>
2015-09-28 17:50:30 -0400

    Added math function

--- hello.py
+++ hello.py
@@ -5,3 +5,7 @@ import helper
 if __name__ == "__main__":
     message = "hello"
     helper.greeting(message)
+    x = 25
+    y = 30
+    mathematics = helper.calc(x, y)
+    print(mathematics)
--- helper.py
+++ helper.py
@@ -2,3 +2,9 @@ __author__ = 'gqt4ev'
 
 def greeting(msg):
     print(msg)
+
+
+def calc(num1, num2):
+    nums = [num1 + num2, num1 - num2, num1 * num2, num1 / num2, num1 % num2]
+    return nums
+
